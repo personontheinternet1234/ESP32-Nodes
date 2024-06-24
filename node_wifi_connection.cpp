75d9c5c3736c9eb81335d4e5e5366c5fb14cc1aa
personontheinternet1234 <135486733+personontheinternet1234@users.noreply.github.com>
2024-06-24 13:40:08 -1000

    publishing more values and  some additional reporting

--- node_wifi_connection.cpp
+++ node_wifi_connection.cpp
@@ -23,18 +23,34 @@ IPAddress server(192,168,0,137);
 
 
 
-void messageCb(const geometry_msgs::Twist& cmd_msg) {
-  float linear_x = cmd_msg.linear.x;
-  float angular_z = cmd_msg.angular.z;
-  // Process the received velocities (linear_x, angular_z) here
-  // For example, send these values to motor controllers
-  Serial.println(linear_x)
-  Serial.println(angular_z)
+void messageCb(const geometry_msgs::Twist& msg) {
+  float linear_x = msg.linear.x;
+  float linear_y = msg.linear.y;
+  float linear_z = msg.linear.z;
+  float angular_x = msg.angular.x;
+  float angular_y = msg.angular.y;
+  float angular_z = msg.angular.z;
+  Serial.print("linear_x = ");
+  Serial.println(linear_x);
+  Serial.print("linear_y = ");
+  Serial.println(linear_y);
+  Serial.print("linear_z = ");
+  Serial.println(linear_z);
+  Serial.print("angular_x = ");
+  Serial.println(angular_x);
+  Serial.print("angular_y = ");
+  Serial.println(angular_y);
+  Serial.print("angular_z = ");
+  Serial.println(angular_z);
 }
 
 
 
 ros::Subscriber<geometry_msgs::Twist> sub("cmd_vel", &messageCb);
+geometry_msgs::Twist twist_msg;
+ros::Publisher cmd_vel_pub("cmd_vel", &twist_msg);
+
+
 
 void setup() {
 
@@ -51,17 +67,38 @@ void setup() {
   }
 
   Serial.println("Connected to network");
+
   nh.getHardware()->setConnection(server, 11411);  // Set your ROS master IP and port
-  // nh.getHardware()->setConnection();  // Set your ROS master IP and port
   nh.initNode();
+
   nh.subscribe(sub);
+  nh.advertise(cmd_vel_pub);
+
+    Serial.println("Setup complete");
 
 }
 
  
 
 void loop() {
+  if (nh.connected()) {
+    // Set the values for the twist message
+    twist_msg.linear.x = 5.0;  // Example linear velocity
+    twist_msg.linear.y = 0;  // Example linear velocity
+    twist_msg.linear.z = 0;  // Example linear velocity
+    twist_msg.angular.x = 0; // Example angular velocity
+    twist_msg.angular.y = 0; // Example angular velocity
+    twist_msg.angular.z = 0; // Example angular velocity
+
+    // Publish the message
+    cmd_vel_pub.publish(&twist_msg);
+
+    // Debug print
+    Serial.println("Published cmd_vel message");
+  } else{
+    Serial.println("Not connected");
+  }
+
   nh.spinOnce();
-  delay(10);
-  Serial.println("looped");
+  delay(1000);
 }
